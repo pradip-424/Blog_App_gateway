70dac6ca96855792eef9ec15c007602ab10d15e2
pradip-424 <106142586+pradip-424@users.noreply.github.com>
2023-11-21 12:03:54 +0530

    Update insert.php

--- crud_p/insert.php
+++ crud_p/insert.php
@@ -1,5 +1,5 @@
 <?php
-include "db_connection.php"; // Include the database connection script
+include "db_connection.php"; 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $title = $_POST["title"];
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql = "INSERT INTO blog_posts (title, content) VALUES ('$title', '$content')";
 
     if ($conn->query($sql) === TRUE) {
-        header("Location: ../index.php"); // Redirect to the blog homepage
+        header("Location: ../index.php"); /
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
