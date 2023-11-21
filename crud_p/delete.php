503a0b59e257a04d79b5fc0b01c9daa32d98f228
pradip-424 <106142586+pradip-424@users.noreply.github.com>
2023-11-21 12:03:29 +0530

    Update delete.php

--- crud_p/delete.php
+++ crud_p/delete.php
@@ -1,11 +1,11 @@
 <?php
-include "db_connection.php"; // Include the database connection script
+include "db_connection.php";
 
 $id = $_GET["id"];
 $sql = "DELETE FROM blog_posts WHERE id=$id";
 
 if ($conn->query($sql) === TRUE) {
-    header("Location:../index.php"); // Redirect to the blog homepage
+    header("Location:../index.php"); 
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
