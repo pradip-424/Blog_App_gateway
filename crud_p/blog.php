6594fd7cb6d53dac9e00ae9fe789e82b63f061d0
pradip-424 <106142586+pradip-424@users.noreply.github.com>
2023-11-21 12:02:53 +0530

    Update blog.php

--- crud_p/blog.php
+++ crud_p/blog.php
@@ -4,13 +4,12 @@ include_once("db_connection.php");
 $id = $_GET["id"];
 
 
-// Include the database connection script
+
 
 $sql = "SELECT * FROM blog_posts where id=$id";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
 ?>
-<!-- HTML for displaying the list of blog posts -->
 
 <?php
 
