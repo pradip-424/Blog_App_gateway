d4b66d8959623f9b9becff995578493dd9341aa1
pradip-424 <106142586+pradip-424@users.noreply.github.com>
2023-11-21 12:03:11 +0530

    Update db_connection.php

--- crud_p/db_connection.php
+++ crud_p/db_connection.php
@@ -1,15 +1,15 @@
 <?php
 
-// Database connection settings (replace with your own)
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "authdb";
 
-// Create a database connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check the connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
